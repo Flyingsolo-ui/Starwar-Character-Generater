9e59445975be138f799865081afbd5c26d328ce8
flyingsolo-ui <lutechintesh09@gmail.com>
2022-07-04 18:45:36 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from "react"
+import "./App.css"
+
+const logo = "https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg"
+const apiUrl = "https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/id"
+
+class StarWars extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: null,
+      height: null,
+      homeworld: null,
+      image: null,
+      loadedCharacter: false
+    }
+  }
+  getNewCharacter() {
+    console.log("Click")
+    const rand = Math.round(Math.random() * 88)
+    const url = `${apiUrl}/${rand}.json`
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          loadedCharacter: true,
+          name: data.name,
+          height: data.height,
+          homeworld: data.homeworld,
+          image: data.image
+        })
+      })
+  }
+  render() {
+    return (
+      <div>
+        {this.state.loadedCharacter && (
+          <div>
+            <h1>{this.state.name}</h1>
+            <img src={this.state.image} alt={this.state.name} height="300" />
+            <p>Height: {this.state.height} m</p>
+            {this.state.homeworld && (
+              <p>
+                Homeworld:
+                <span className="capitalize"> {this.state.homeworld}</span>
+              </p>
+            )}
+          </div>
+        )}
+        <button onClick={() => this.getNewCharacter()} type="button" className="btn">
+          Randomize character
+        </button>
+      </div>
+    )
+  }
+}
+
+class StarWarsTitle extends React.Component {
+  render() {
+    return (
+      <>
+        <img src={logo} className="App-logo" alt="StarWars" />
+      </>
+    )
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <StarWarsTitle />
+      <StarWars />
+    </div>
+  )
+}
+
+export default App
